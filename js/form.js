452b81cdad294a2f6d4e0bcb7390a95d1cea3da9
ToMoises <87343699+ToMoises@users.noreply.github.com>
2022-08-25 21:55:32 -0500

    input_diseño_2

---
+++ js/form.js
@@ -0,0 +1,10 @@
+console.log("entrooooo");
+
+
+
+const Entrada = document.getElementById("entrada");
+const Segundo = document.getElementById("segundo");
+const Bebida = document.getElementById("bebida");
+const Postre = document.getElementById("postre");
+const Precio = document.getElementById("precio");
+
