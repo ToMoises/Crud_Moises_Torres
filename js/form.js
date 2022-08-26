6ac740940f8ab25abca44f208810aecc4d6000d6
ToMoises <87343699+ToMoises@users.noreply.github.com>
2022-08-25 23:30:04 -0500

    create

--- js/form.js
+++ js/form.js
@@ -8,3 +8,27 @@ const Bebida = document.getElementById("bebida");
 const Postre = document.getElementById("postre");
 const Precio = document.getElementById("precio");
 
+
+function crear(){
+    let menu = { 
+        Entrada,
+        Segundo,
+        Bebida,
+        Postre,
+        Precio
+    }
+
+    if (localStorage.getItem("menus") === null) {
+        let menus =[]
+        menus.push(menu)
+        localStorage.setItem("menus",JSON.stringify(menus))
+    } else{
+        let menus = localStorage.getItem(JSON.parse("menus"))
+        menus.push(menu)
+        localStorage.setItem("menus",JSON.stringify(menus))
+    }
+
+    
+        
+}
+
